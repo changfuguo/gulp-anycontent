6a8ec4f92d76dd2262efb8648169165b8a40046b
changfuguo <changfuguo@qq.com>
2015-08-21 11:44:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,18 @@ var through2 = require('through2');
 
 module.exports = function (fnProcess){
 	function process(file,enc,callback) {
-	
 		if(file.isNull()){
-		
 			this.push(file);
 			return callback();
 		}
-
 		if(file.isStream()) {
-			 return this.emit('error', new gutil.PluginError('gulp-assetpaths',  'Streaming not supported'));
+			 return this.emit('error', new gutil.PluginError('gulp-anycontent',  'Streaming not supported'));
 		}
 		if(file.isBuffer()){
 			var outfileContents = '';
 			var contents = file.contents.toString('utf8');
-			
 			if(fnProcess && typeof fnProcess == 'function'){
-			
-				contents = fnProcess(contents, file.path);
+				contents = fnProcess(contents, file.path) || contents;
 			}
 			var outfile = file.clone();
 			outfile.contents = new Buffer(outfileContents);
